4e1e5592a4690a7d933e1500e43857f043d816d3
striggle <stbigrigg@gmail.com>
2018-08-29 11:56:42 -0400

    sql day3 commit

---
+++ day3-sql script.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM product
+where listprice = 57.50
+and code = 'java';
+
+select * from product
+where listprice < 55;
+
+select * from product
+order by description desc;
+
+select max(listprice) from product
+
+
+
